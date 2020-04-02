2fcbddf48ae8df8e6940b280c5221243e6d6327a
shymanovich <shymanovichvlad@gmail.com>
2020-04-03 00:10:04 +0300

    Update jsonReader.scala

--- src/main/scala/jsonReader.scala
+++ src/main/scala/jsonReader.scala
@@ -3,13 +3,13 @@ import org.json4s._
 import org.json4s.jackson.JsonMethods._
 
 case class wineryProduction(
-                 country: String,
-                 id: Int,
-                 points: Int,
-                 price: Int, 
-                 title: String,
-                 variety: String,
-                 winery: String)
+                 country: Option[String],
+                 id: Option[Int],
+                 points: Option[Int],
+                 price: Option[Int],
+                 title: Option[String],
+                 variety: Option[String],
+                 winery: Option[String])
 
 object jsonReader {
   val conf = new SparkConf().setAppName("JsonReader")
@@ -19,7 +19,7 @@ object jsonReader {
   def main(args: Array[String]): Unit = {
     val text = sc.textFile(args(0))
 
-    text.foreach(x => {implicit val formats = DefaultFormats; println(parse(x).extractOpt[wineryProduction])})
+    text.foreach(x => {implicit val formats = DefaultFormats; println(parse(x).extract[wineryProduction].price)})
 
   }
 }
